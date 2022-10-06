1a9656d5462f1d66ef35980885155d2580aa7be9
anushkakaushik <79746679+anushkakaushik@users.noreply.github.com>
2022-10-06 16:44:49 +0530

    Update LeapYear.java

--- 6-10-22/LeapYear.java
+++ 6-10-22/LeapYear.java
@@ -11,14 +11,17 @@ class LeapYear
 	        int year = s.nextInt();
 	        boolean check = false;
 	        if(year % 400 == 0)
+			// checks if the year is divisible by 400
 	        {
 	            check = true;
 	        }
 	        else if (year % 100 == 0)
+			// this condition checks if the year entered by the user is divisible by 100
 	        {
 	            check = false;
 	        }
 	        else if(year % 4 == 0)
+			// checks if the year gets remainder after modulo operator 4 
 	        {
 	            check = true;
 	        }
@@ -27,10 +30,12 @@ class LeapYear
 	            check = false;
 	        }
 	        if(check)
+			// prints leap year if check is  true
 	        {
 	            System.out.println("Year "+year+" is a Leap Year");
 	        }
 	        else
+			// prints not a leap year if check is false
 	        {
 	            System.out.println("Year "+year+" is not a Leap Year");
 	        }
