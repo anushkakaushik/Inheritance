8051846db13d9d16010411784e813db9dba36d52
anushkakaushik <79746679+anushkakaushik@users.noreply.github.com>
2022-10-07 16:18:41 +0530

    Update Calculation.java

--- 29-9-22/Calculation.java
+++ 29-9-22/Calculation.java
@@ -1,4 +1,15 @@
 package CoreJava;
+/*
+Program: Create a program for calculation.read three values from the user for operation
+first and second values as operands.
+third value as operator.
+if user press1 => print addition of first and second
+if user press2 => print subtraction of first and second
+if user press3 => print multiplication of first and second
+if user press4 => print division of first and second
+@author: Anushka kaushik
+
+*/
 import java.util.Scanner;
  class Calculation {
 public static  void main(String [] args) {
