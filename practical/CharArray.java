a6f173db0d891e169aaa4ca1fe8efe9669339746
anushkakaushik <79746679+anushkakaushik@users.noreply.github.com>
2022-10-19 15:16:56 +0530

    Add files via upload

---
+++ practical/CharArray.java
@@ -0,0 +1,13 @@
+package CoreJava;
+
+public class CharArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a[] = {'a','b','c'};
+		for (int i=0;i<a.length;i++) {
+			System.out.println(a);
+		}
+	}
+
+}
